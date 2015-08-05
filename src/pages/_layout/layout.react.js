04c31d117e0e43687a21174a0f225edef8d48b4c
Martin Katrenik <martin.katrenik@merck.com>
2015-08-05 16:56:51 +0200

    update(layout): simplify

--- src/pages/_layout/layout.react.js
+++ src/pages/_layout/layout.react.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import logo from '../../assets/images/msd_logo-symbol-only_white.svg';
+// HINT: `flexboxgrid` uses same classnames as bootstrap
 import 'flexboxgrid/dist/flexboxgrid';
 import styles from './styles.sass';
 
@@ -9,22 +10,16 @@ export default class Layout extends Component {
   render() {
     return (
       <div className={`${styles.navbar} row middle-xs`}>
-        <div className="col-lg-4">
-          <div className="box">
-            menu, app name
-          </div>
+        <div className="col-xs-3">
+          menu, app name
         </div>
-        <div className="col-lg-4">
-          <div className="box center-xs">
-            <a className={styles.logo} href="#">
-              <img src={logo} />
-            </a>
-          </div>
+        <div className="col-xs-6 center-xs">
+          <a className={styles.logo} href="#">
+            <img src={logo} />
+          </a>
         </div>
-        <div className="col-lg-4">
-          <div className="box end-xs">
-            settings
-          </div>
+        <div className="col-xs-3 end-xs">
+          settings
         </div>
         {this.props.children}
       </div>
